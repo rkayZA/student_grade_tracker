ce2e9f5f4fb6998865d262c57837b12b13a6d872
Robert Klaic <robert.klaic@outlook.com>
2024-06-14 08:44:57 +0200

    Added comments

--- student_tracker.py
+++ student_tracker.py
@@ -33,18 +33,20 @@ def main():
     elif user_selection == '3':
         sys.exit("Quitting")
 
-
+# Clears the console window
 def clear_console():
     if os.name =='nt':
         os.system("cls")
     else:
         os.system("clear")
 
+# Returns to the main menu
 def return_to_main():
     input("Press ENTER to return to the MAIN MENU")
     clear_console()
     main()
 
+# Updates a specific student exam record
 def update_student_record(records):
     list_students(records)
     student = input("Please select student number to update: ").upper()
@@ -53,29 +55,27 @@ def update_student_record(records):
         print(student, "is not a valid student number")
         student = input("Please select student number to update: ").upper()
     print()
-    
+
     print("Student", student, "exam scores")
     list_student_scores(records, student)
-        
+
     print("\n5. Return to student list\n6. Return to main menu")
-    
+
     exam_selection = input("\nSelect exam to update score (1, 2, 3, 4): ")
 
     while exam_selection not in ['1', '2', '3', '4', '5', '6']:
         print(exam_selection,"is not a valid selection")
         exam_selection = input("Select exam to update score (1, 2, 3, 4): ")
-    
+
     if exam_selection =='5':
         update_student_record(records)
     elif exam_selection == '6':
-       main()
+        main()
     else:
         exam_number = "exam_" + exam_selection
         update_exam_record(records, student, exam_number)
-    
-    
-    
 
+# makes changes to student exam record
 def update_exam_record(student_records,student, exam):
     print("Updating", exam, "for student", student.upper())
     score = input("Enter the score for " + exam + ": ")
@@ -92,7 +92,7 @@ def update_exam_record(student_records,student, exam):
 
     return_to_main()
 
-# Display a list of students with their exam scores
+# Display a list of students, with or without the exam records
 def list_students(student_records, include_exam = False):
 
     for student_no, student_info in student_records.items():
@@ -103,15 +103,13 @@ def list_students(student_records, include_exam = False):
             print("Exam 3:", student_info["exam_3"])
             print("Exam 4:", student_info["exam_4"])
             print()
-    
-    
 
 # Display a list of exam scores for a particular student
 def list_student_scores(student_records, student_number):
     for i in range (1, 5):
         print("Exam "+ str(i) + ":", str(student_records[student_number]["exam_" + str(i)]))
 
-
+# Save data to file
 def save_to_file(student_records):
     with open(STUDENT_RECORD_FILE, "w") as student_file:
         json.dump(student_records, student_file) 
@@ -120,9 +118,10 @@ def save_to_file(student_records):
 def load_from_file(file_name):   
     with open(file_name) as json_file:
         records = json.load(json_file)
-    
+
     return records
 
+# The main menu
 def main_menu():
     selected_option = ''
     print("*** MAIN MENU ***")
@@ -140,8 +139,6 @@ def main_menu():
 
 def header():
     print("STUDENT EXAM RECORD TRACKER")
-
-
 
 
 # Python boilerplate.
